9c3cbf9165aa540e87f2a752c82c8307b0ffe204
Daniel Choi <dhchoi@gmail.com>
2012-12-26 15:02:20 -0500

    Code for generating a Kindle edition of the 1st three volumes

---
+++ kindle/README.md
@@ -0,0 +1,18 @@
+## A Kindle edition generator for Practicing Ruby
+
+The generate.rb script will generate a Kindle-compatible MOBI document that
+you can copy to your Kindle's document folder or send to your Kindle email
+address  and then read on your Kindle. 
+
+To run generate.rb, please first install the `kindlerb` gem and the
+`kindlegen` binary. Instructions are
+[here](https://github.com/danchoi/kindlerb).
+
+Then from this `kindle` subdirectory, run this command:
+
+    ruby generate.rb
+
+You should see some output, and then see a file generated called something like `practicing-ruby-2012-12-26.mobi`. Transfer this file to your Kindle. 
+
+
+
---
+++ kindle/generate.rb
@@ -0,0 +1,74 @@
+require 'yaml'
+require 'pathname'
+require 'github/markdown' # gem install github-markdown
+if `which kindlerb` == ''
+  abort "Please run `gem install kindlerb`"
+end
+
+# Run this from the kindle/ subdirectory
+
+
+def wrap_html(title, body)
+  "<html><head><title>#{title}</title></head><body>#{body}</body></html>"
+end
+
+
+cover_path = nil
+date = Time.now.strftime('%F')
+document = {
+  'doc_uuid' => "Practicing-Ruby-#{date}",
+  'title' => "Practicing Ruby",
+  'author' => "Gregory Brown",
+  'publisher' => "Gregory Brown",
+  'subject' => "Ruby Programming",
+  'date' => date,
+  'masthead' => nil,
+  'cover' => cover_path,
+  'mobi_outfile' => "practicing-ruby-#{date}.mobi"
+}
+
+puts document.inspect
+File.write("_document.yml", document.to_yaml)
+
+# loop over the volumes and turn them into sections
+
+path  = Pathname.new("../articles")
+sections = path.children
+
+`mkdir -p sections`
+sections.each_with_index {|vol, i|
+  puts "Processing #{vol.to_s}"
+
+  vol_toc = File.readlines(vol + "README.md").select{|line| line =~ /Issue/}.reduce({}) {|memo,x|
+    key = x[/Issue \d+\.([^:]+)/,1].gsub('.', '')
+    title = x[/: ([^\]]+)/, 1]
+    memo[key] = title
+    memo
+  }
+
+  puts vol_toc 
+
+  spath = "sections/%.3d" % i
+  `mkdir -p #{spath}`
+
+  # save section title in file
+  stitle = "Volume #{i + 1}"
+  File.write(spath + '/_section.txt', stitle)
+
+  # process each article
+  vol.children.delete_if {|x| x.to_s =~ /README/}.each_with_index {|article, j| 
+    puts article.to_s
+    apath = "sections/%.3d/%.3d.html" % [i, j]
+    md_content = File.read article    
+    html_content = GitHub::Markdown.render_gfm(md_content)
+    html_content = GitHub::Markdown.render(md_content)
+    title_key = article.to_s[/\/(\d{3}\w?)/, 1].sub(/0+/, '') # strip leading 0s
+    title = vol_toc[title_key]
+    raise "No title found for #{title_key}" unless title
+    puts title
+    html = wrap_html(title, "<h1>#{title}</h1>" + html_content)
+    File.write(apath, html)
+  }
+}
+
+exec "kindlerb"
